a10c810bb797a2b474b49fdd3b421574ecdf9b89
dukang <625079860@qq.com>
2018-05-17 20:42:18 +0800

    优化中国天气网查询

--- china_weather.py
+++ china_weather.py
@@ -14,6 +14,7 @@ CITY_LIST = []
 MIN_LIST = []
 
 
+# 获取当前的数据 ，并且存入字典
 def get_temperature(url):
     headers = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36"
@@ -27,7 +28,6 @@ def get_temperature(url):
 
     for conMidtab2_list in conMidtab2_lists:
         tr_list = conMidtab2_list.find_all('tr')[2:]  # 从第二个元素之后开始
-        province = ''
 
         for index, tr in enumerate(tr_list):
             if index == 0:
@@ -44,10 +44,43 @@ def get_temperature(url):
                 'city': city,
                 'min': min
             })
-            # CITY_LIST.append(province + city)
-            # MIN_LIST.append(min)
 
-            # print '%s|%s' % (province + city, min)
+
+# 获取所有城市的最低温度
+def get_temperature_result(urls):
+    for url in urls:
+        get_temperature(url)
+        time.sleep(2)
+
+
+# 将获取到的所有城市数据存入json
+def save_temperature_json():
+    line = json.dumps(TEMPER_LIST, ensure_ascii=False)
+    with open('temperature.json', 'w') as fp:
+        fp.write(line.encode('utf-8'))
+
+
+# 从json文件获得所有的城市数据
+def get_temperature_json():
+    with open('temperature.json', 'r') as fp:
+        return json.load(fp, encoding='utf-8')
+
+
+# 将数据进行处理，获取到最低温度的前20名 进行展示
+def data_visualization(temperature_list):
+    top20_temperature_list = sorted(temperature_list, lambda x, y: cmp(int(x['min']), int(y['min'])))[0:20]
+    city20_list = []
+    min20_list = []
+    for city_min in top20_temperature_list:
+        city20_list.append(city_min['city'])
+        min20_list.append(city_min['min'])
+
+    echart_show = Echart(u'全国最低温度排名', u'小白打酱油提供', u'这是气温排名')
+    bar = Bar(u'最低温度', min20_list)
+    axis = Axis('category', 'bottom', data=city20_list)
+    echart_show.use(bar)
+    echart_show.use(axis)
+    echart_show.plot()
 
 
 def main():
@@ -60,33 +93,16 @@ def main():
         'http://www.weather.com.cn/textFC/xb.shtml',
         'http://www.weather.com.cn/textFC/xn.shtml'
     ]
-
-    for url in urls:
-        get_temperature(url)
-        time.sleep(2)
-
-    TOP20_TEMPERATURE_LIST = sorted(TEMPER_LIST, lambda x, y: cmp(int(x['min']), int(y['min'])))[0:20]
-
-    # CITY20_LIST = CITY_LIST[0:20]
-    # MIN20_LIST = MIN_LIST[0:20]
-
-    CITY20_LIST = []
-    MIN20_LIST = []
-
-    for city_min in TOP20_TEMPERATURE_LIST:
-        CITY20_LIST.append(city_min['city'])
-        MIN20_LIST.append(city_min['min'])
-    line = json.dumps(TEMPER_LIST, ensure_ascii=False)
-    with open('temperatu.json', 'w') as fp:
-        fp.write(line.encode('utf-8'))
-
-    echart = Echart(u'全国最低温度排名', u'提供')
-    bar = Bar(u'最低温度', MIN20_LIST)
-    axis = Axis('category', 'bottom', data=CITY20_LIST)
-    echart.use(bar)
-    echart.use(axis)
-    echart.plot()
+    # 通过爬虫获取相关的数据
+    # get_temperature_result(urls)
+    # 将爬虫获取到的数据放入到json
+    # save_temperature_json()
+    # 从json文件中获取到爬虫数据
+    temperature_list = get_temperature_json()
+    # 进行数据展示
+    data_visualization(temperature_list)
 
 
+# 设置默认启动为main函数
 if __name__ == '__main__':
     main()
